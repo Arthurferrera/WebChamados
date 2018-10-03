87d72768384c6d3dcf4f9dc904dc44dd0b6dd921
Guilherme <teste@teste.com>
2018-10-03 11:35:26 -0300

    melhorias ns apis e layout

--- APIChamados/deslogar.php
+++ APIChamados/deslogar.php
@@ -1,18 +1,25 @@
 <?php
+    // require do arquivo de conexao
     require_once('conexao.php');
+
+    // resgatando parametros da url
     $id = $_GET['id'];
     $nivel = $_GET['nivel'];
 
+    // verificando qual o nivel de usuario
     if ($nivel == "Cliente") {
         $update = "UPDATE usuario SET logado = 0 WHERE id =".$id;
     } else if($nivel == "Administrador"){
         $update = "UPDATE usuarioAdm SET logado = 0 WHERE id =".$id;
     }
+
+    // executando o comando no banco
     $stm = sqlsrv_query($conexao, $update);
+
+    // verificando se foi executado, para retornar algum valor
     if ($stm) {
         echo json_encode(array("sair" => true));
     } else {
         echo json_encode(array("sair" => false));
     }
-
  ?>
--- APIChamados/login.php
+++ APIChamados/login.php
@@ -4,15 +4,15 @@
     // resgatando os parametros da url
     $usuario = $_GET['usuario'];
     $senha = $_GET['senha'];
-    // comando sql
+
     //este select 'converte' o registro do BD, para binario
     // possibilitando a diferenciação de letras maiúsculas e minúsculas
-    $tsql = "SELECT u.id, u.cnpj, u.razaoSocial, u.nome, u.usuario, u.senha, u.idNivelUsuario, n.nivel, n.idNivelUsuario
+    $tsql = "SELECT u.id, u.cnpj, u.razaoSocial, u.nome, u.usuario, u.senha, u.idNivelUsuario, n.nivel, n.idNivelUsuario, u.logado
             FROM usuario AS u
             INNER JOIN nivelUsuario AS n
             ON u.idNivelUsuario = n.idNivelUsuario
             WHERE BINARY_CHECKSUM(usuario) = BINARY_CHECKSUM('$usuario')
-            AND BINARY_CHECKSUM(senha) = BINARY_CHECKSUM('$senha') AND u.logado != 1";
+            AND BINARY_CHECKSUM(senha) = BINARY_CHECKSUM('$senha')";
     // executando o comando no banco
     $stm = sqlsrv_query($conexao, $tsql);
 
@@ -20,24 +20,37 @@
     // caso sim retorna o objeto usuario, e permite o login
     // caso contrario, não permite o login
     if (sqlsrv_has_rows($stm) > 0 && $usuario = sqlsrv_fetch_object($stm)) {
-        echo json_encode(array("login" => true, "usuario" => $usuario));
-        $update = "UPDATE usuario set logado = 1 where id =".$usuario->id;
-        sqlsrv_query($conexao, $update);
+        if($usuario->logado){
+            echo json_encode(array("login" => false, "logado" => true));
+        } else {
+            echo json_encode(array("login" => true, "usuario" => $usuario));
+            // mudo o status de login para true, para que o usuario tenha apenas 1 login ativo
+            $update = "UPDATE usuario set logado = 1 where id =".$usuario->id;
+            sqlsrv_query($conexao, $update);
+        }
     } else {
-        // echo json_encode(array("login" => false));
-        $tsqlAdm = "SELECT u.id, u.nome, u.login, u.senha, u.idNivelUsuario, n.nivel, n.idNivelUsuario
+        // select da tabela de adms
+        $tsqlAdm = "SELECT u.id, u.nome, u.login, u.senha, u.idNivelUsuario, n.nivel, n.idNivelUsuario, u.logado
                 FROM usuarioAdm AS u
                 INNER JOIN nivelUsuario AS n
                 ON u.idNivelUsuario = n.idNivelUsuario
                 WHERE BINARY_CHECKSUM(login) = BINARY_CHECKSUM('$usuario')
-                AND BINARY_CHECKSUM(senha) = BINARY_CHECKSUM('$senha') AND u.logado != 1";
+                AND BINARY_CHECKSUM(senha) = BINARY_CHECKSUM('$senha')";
+        // executa a query
         $stmAdm = sqlsrv_query($conexao, $tsqlAdm);
+
+        // verifica se retornou alguma linha e pega o objeto retornado
         if (sqlsrv_has_rows($stmAdm) > 0 && $usuario = sqlsrv_fetch_object($stmAdm)) {
-            $update = "UPDATE usuarioAdm set logado = 1 where id =".$usuario->id;
-            sqlsrv_query($conexao, $update);
-            echo json_encode(array("login" => true, "usuario" => $usuario));
+            if($usuario->logado){
+                echo json_encode(array("login" => false, "logado" => true));
+            } else {
+                // mudo o status de login para true, para que o usuario tenha apenas 1 login ativo
+                $update = "UPDATE usuarioAdm set logado = 1 where id =".$usuario->id;
+                sqlsrv_query($conexao, $update);
+                echo json_encode(array("login" => true, "usuario" => $usuario));
+            }
         } else {
-            echo json_encode(array("login" => false));
+            echo json_encode(array("login" => false, "logado" => false));
         }
     }
 ?>
